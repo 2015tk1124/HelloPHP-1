aa1f4457f695d8c4bc1b95e611e09b9d009f3eb0
maru0315 <maru0315.000@gmail.com>
2015-06-22 15:00:02 +0900

    ファイルを追加したＹＯ!!!!!

---
+++ HelloPHP.php
@@ -0,0 +1,29 @@
+<?php
+print "HelloPHP!<br>";
+print "小島っす！";
+print "	<table>
+			<tr>
+				<th>性別</th>
+				<td>男性</td>
+			</tr>
+			<tr>
+				<th>年齢</th>
+				<td>２４歳</td>
+			</tr>
+			<tr>
+				<th>血液型</th>
+				<td>AB型</td>
+			</tr>
+		</table>";
+print "<style>
+			*{
+				background:rgba(0,255,0,0.1);
+				font-size:20px;
+			}
+			table,th,td{
+				border:1px solid #000000;	
+				background:#ffffff;
+				width:200px;
+			}
+		</style>";
+?>
